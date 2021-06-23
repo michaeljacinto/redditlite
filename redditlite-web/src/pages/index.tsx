03955cf1729698d8bd5e00d33b1b3a56791fb395
michaeljacinto <atmwhockey@hotmail.com>
2021-06-23 16:25:02 -0700

    Add changes

--- redditlite-web/src/pages/index.tsx
+++ redditlite-web/src/pages/index.tsx
@@ -1,13 +1,21 @@
 import { withUrqlClient } from "next-urql"
 import React from "react"
 import { NavBar } from "../components/NavBar"
+import { usePostsQuery } from "../generated/graphql"
 import { createUrqlClient } from "../utils/createUrqlClient"
 
-const Index = () => (
-  <>
-    <NavBar></NavBar>
-    <div>Hello world!</div>
-  </>
-)
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar></NavBar>
+      <div>Hello world!</div>
+      {!data ? (
+        <div>Loading ...</div>
+      ) : (data.posts.map((p) => <div key={p._id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
 
-export default withUrqlClient(createUrqlClient)(Index);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
